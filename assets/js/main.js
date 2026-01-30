462136399e123ce85b59612ff4727ac0adc12f29
aadyantnarula15-stack <aadyantnarula15@gmail.com>
2026-01-31 01:36:39 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,4 +255,18 @@
 
 		}
 
+
 })(jQuery);
+// Founder section scroll animation
+$(window).on('scroll', function () {
+	var founderSection = $('#founder');
+
+	if (founderSection.length) {
+		var sectionTop = founderSection.offset().top;
+		var scrollPos = $(window).scrollTop() + $(window).height() * 0.8;
+
+		if (scrollPos > sectionTop) {
+			founderSection.addClass('visible');
+		}
+	}
+});
